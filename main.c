130941ac8dc154a669009cee227012564ccc8683
Tonmoy <tonmoy@4axiz.com>
2021-02-20 23:05:36 +0600

    init 3

--- main.c
+++ main.c
@@ -3,9 +3,33 @@
 
 int main()
 {
+
+
+    char password[100] = "";
+    printf("Please enter the password for encrypt question file\n");
+    printf("\nEnter Password: ");
+   while(1){
+
+
+    inputPassword(password);
+    if(!strlen(password)<=0){
+       break;
+    }else{
+        printf("\nEnter valid password: ");
+    }
+
+   }
+   printf("\n");
+   char com[] = "7z a -t7z txt.7z e\* -p";
+   strcpy(com,password);
+
+   printf("%d",com);
+
    openFile();
    addQuestion();
    fileClose();
+
+
 
     return 0;
 }
